dc961635a8160da86a7c3f2ba71ddc091c29ded9
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2012-01-03 15:50:35 +0100

    Additional tests for BrewMethod

--- CoffeeTimerTests/BrewMethodTests/BrewMethodTests.h
+++ CoffeeTimerTests/BrewMethodTests/BrewMethodTests.h
@@ -16,4 +16,6 @@
     
 }
 
+@property (nonatomic, retain) BrewMethod *bm;
+
 @end
--- CoffeeTimerTests/BrewMethodTests/BrewMethodTests.m
+++ CoffeeTimerTests/BrewMethodTests/BrewMethodTests.m
@@ -9,7 +9,12 @@
 #import "BrewMethodTests.h"
 #import "BrewMethod.h"
 
-@implementation BrewMethodTests
+#define NUM_STEPS 300
+#define MAX_TIME 200
+
+@implementation BrewMethodTests 
+
+@synthesize bm;
 
 - (void)setUp
 {
@@ -76,6 +81,55 @@
 
 - (void)testStress
 {
+    NSArray *verbs = [[NSArray alloc] initWithObjects:@"Work", @"Make", @"Do", @"Pour", @"Drink", nil];
+    NSArray *pnoun = [[NSArray alloc] initWithObjects:@"It", @"That", @"This", @"Something", @"Anything", nil];
+    
+    int verbsLen = [verbs count];
+    int pnounLen = [pnoun count];
+
+    NSMutableArray *descriptions = [[NSMutableArray alloc] init];
+    NSMutableArray *times        = [[NSMutableArray alloc] init];
+    NSMutableArray *steps        = [[NSMutableArray alloc] init];
+    
+    int time, totalTime;
+    NSString *str;
+    // Create a number of timer steps
+    for (int i = 0; i < NUM_STEPS; ++i) {
+        str = [NSString stringWithFormat:@"%@ %@", 
+                         [verbs objectAtIndex:(rand() % verbsLen)],
+                         [pnoun objectAtIndex:(rand() % pnounLen)]];
+        
+        [descriptions addObject:str];
+        
+        time = (rand() % MAX_TIME);
+        totalTime += time;
+        [times addObject:[NSNumber numberWithInt:time]];
+        
+        [steps addObject:[[TimerStep alloc] initWithDescription:str 
+                                                  timeInSeconds:time]];
+    }
+    
+    STAssertEquals([bm totalTimeInSeconds], totalTime, @"Total time mismatch");
+    
+    
+    BrewMethod *bm2 = [[BrewMethod alloc] initWithName:@"Test"
+                                        timerStepArray:steps];
+    
+    int index;
+    // Make sure that the steps in the method matches the generated steps
+    for (int i = 0; i < NUM_STEPS; ++i) {
+        index = rand() % NUM_STEPS;
+        STAssertEqualObjects([[bm2.timerSteps objectAtIndex:index] descriptionWithoutTime], [descriptions objectAtIndex:index], @"Mismatch between descriptions at index %d", index);
+        STAssertEquals([[bm2.timerSteps objectAtIndex:index] timeInSeconds], [[times objectAtIndex:index] intValue], @"Mismatch between times at index: %d", index);
+    }
+    
+    [descriptions release];
+    [times release];
+    [steps release];
+    
+    bm2.timerSteps = nil;
+    STAssertNil([bm firstTimerStep], @"First timer step is not nil after removing steps");
+    STAssertNil([bm nextTimerStep], @"Next timer step is not nil after removing steps");
     
 }
 
